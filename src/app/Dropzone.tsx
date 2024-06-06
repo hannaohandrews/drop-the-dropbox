93410f0e2b70af511b260509bb4b5f35f3272f21
Hanna Oh Andrews <63070798+hannaohandrews@users.noreply.github.com>
2024-06-05 17:53:36 -0700

    preview, accepted files, rejected files

--- src/app/Dropzone.tsx
+++ src/app/Dropzone.tsx
@@ -1,22 +1,135 @@
 "use client";
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { ArrowUpTrayIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
-const Dropzone = () => {
-  const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
+interface DropzoneProps {
+  className?: string;
+}
+
+const Dropzone: React.FC<DropzoneProps> = ({ className }) => {
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onDrop = useCallback((acceptedFiles: any) => {
+    if (acceptedFiles?.length) {
+      setFiles((previousFiles) => [
+        ...previousFiles,
+        ...acceptedFiles.map((file: any) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        ),
+      ]);
+    }
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
+  const removeFile = (name: string) => {
+    setFiles((files) => files.filter((file) => file.name !== name));
+  };
+
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
-    </div>
+    <form>
+      <div {...getRootProps()} className={className}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag and drop some files here, or click to select files</p>
+        )}
+      </div>
+
+      {/*preview */}
+      <section className="mt-10">
+        <div className="flex gap-4">
+          <h2 className="title text-3xl font-semibold">Preview</h2>
+          <button
+            type="button"
+            // onClick={removeAll}
+            className="mt-1 text-[12px] uppercase tracking-wider font-bold text-neutral-500 border border-secondary-400 rounded-md px-3 hover:bg-secondary-400 hover:text-white transition-colors"
+          >
+            Remove all files
+          </button>
+          <button
+            type="submit"
+            className="ml-auto mt-1 text-[12px] uppercase tracking-wider font-bold text-neutral-500 border border-purple-400 rounded-md px-3 hover:bg-purple-400 hover:text-white transition-colors"
+          >
+            Upload to Cloudinary
+          </button>
+        </div>
+        <ul>
+          {files.map((file) => (
+            <li key={file.name}>
+              <img
+                src={file.preview}
+                style={{ width: "100px", height: "100px" }}
+              />
+              {file.name}
+            </li>
+          ))}
+        </ul>
+
+        {/* Accepted files */}
+        <h3 className="title text-lg font-semibold text-neutral-600 mt-10 border-b pb-3">
+          Accepted Files
+        </h3>
+        <ul className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-10">
+          {files.map((file) => (
+            <li key={file.name} className="relative h-32 rounded-md shadow-lg">
+              <img
+                src={file.preview}
+                alt={file.name}
+                width={100}
+                height={100}
+                onLoad={() => {
+                  URL.revokeObjectURL(file.preview);
+                }}
+                className="h-full w-full object-contain rounded-md"
+              />
+              <button
+                type="button"
+                className="w-7 h-7 border border-secondary-400 bg-secondary-400 rounded-full flex justify-center items-center absolute -top-3 -right-3 hover:bg-white transition-colors"
+                title="Remove File"
+                // onClick={() => removeFile(file.name)}
+              >
+                <XMarkIcon className="w-5 h-5 fill-white hover:fill-secondary-400 transition-colors" />
+              </button>
+              <p className="mt-2 text-neutral-500 text-[12px] font-medium">
+                {file.name}
+              </p>
+            </li>
+          ))}
+        </ul>
+
+        {/* Rejected Files */}
+        <h3 className="title text-lg font-semibold text-neutral-600 mt-24 border-b pb-3">
+          Rejected Files
+        </h3>
+        <ul className="mt-6 flex flex-col">
+          {/* {rejected.map(({ file, errors }) => (
+            <li key={file.name} className="flex items-start justify-between">
+              <div>
+                <p className="mt-2 text-neutral-500 text-sm font-medium">
+                  {file.name}
+                </p>
+                <ul className="text-[12px] text-red-400">
+                  {errors.map((error) => (
+                    <li key={error.code}>{error.message}</li>
+                  ))}
+                </ul>
+              </div>
+              <button
+                type="button"
+                className="mt-1 py-1 text-[12px] uppercase tracking-wider font-bold text-neutral-500 border border-secondary-400 rounded-md px-3 hover:bg-secondary-400 hover:text-white transition-colors"
+                onClick={() => removeRejected(file.name)}
+              >
+                remove
+              </button>
+            </li>
+          ))} */}
+        </ul>
+      </section>
+    </form>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Dropzone from "./Dropzone";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>DROP THE DROPBOX</h1>
       <Dropzone />
     </div>
   );
